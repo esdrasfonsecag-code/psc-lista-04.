6fe5f64054eaf3c544808406ff17733d6145a2e7
Esdras <esdrasfonsecag@gmail.com>
2025-10-06 07:57:46 -0300

    Create Exercicio09.java

---
+++ Exercicio09.java
@@ -0,0 +1,8 @@
+public class Exercicio09 {
+    public static void main(String[] args) {
+        System.out.println("Números ímpares entre 1 e 50:");
+        for (int i = 1; i <= 50; i += 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
