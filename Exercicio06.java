d0ffa58ea62823efae3b5cf71b537cac2baa59c5
Esdras <esdrasfonsecag@gmail.com>
2025-10-06 07:55:40 -0300

    Exercicio06.java

---
+++ Exercicio06.java
@@ -0,0 +1,15 @@
+public class Exercicio06 {
+    public static void main(String[] args) {
+        // Números um abaixo do outro
+        System.out.println("Números um por linha:");
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+
+        // Números lado a lado
+        System.out.println("\nNúmeros na mesma linha:");
+        for (int i = 1; i <= 20; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
